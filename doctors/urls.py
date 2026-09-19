ca6a8970a4a7eddf7c787051ea651af81f02f1b4
Aravinth Govindaraj <aravinthgovindaraj143@gmail.com>
2026-09-19 15:53:55 +0530

    Fix doctor profile image

--- doctors/urls.py
+++ doctors/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.doctors_list, name='doctors_list'),
+    path('treatments/', views.treatments, name='treatments'),
     path('specialties/', views.specialties, name='specialties'),
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
-from .models import Doctor
+from .models import Doctor, Treatment
+
+
+def doctors_list(request):
+	doctors = Doctor.objects.filter(
+		is_active=True
+	).prefetch_related(
+		"specialities",
+		"treatments",
+		"hospitals",
+	).order_by("created_at", "full_name")
+
+	return render(
+		request,
+		"doctors/doctors_list.html",
+		{"doctors": doctors},
+	)
+
+
+def treatments(request):
+	treatments = Treatment.objects.prefetch_related(
+		"doctor_set",
+	).order_by("name")
+
+	return render(
+		request,
+		"doctors/treatments.html",
+		{"treatments": treatments},
+	)
 
 
 def specialties(request):
