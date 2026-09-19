7c4bcc24e4a32210d6d603e1f63425b8196620b7
Aravinth Govindaraj <aravinthgovindaraj143@gmail.com>
2026-09-19 12:29:58 +0530

    Add specialties page route

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Doctor
 
-# Create your views here.
+
+def specialties(request):
+	doctor = Doctor.objects.filter(is_active=True).prefetch_related(
+		"specialities"
+	).first()
+
+	return render(
+		request,
+		"doctors/specialties.html",
+		{
+			"doctor": doctor,
+			"specialties": doctor.specialities.all() if doctor else [],
+		},
+	)
